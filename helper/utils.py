f10b9dcd1daeaa001c07cad7e4494b1f6e26f21f
patrickv93 <patrick.vacek.93@gmail.com>
2022-11-18 22:46:49 -0800

    add csv loading step

---
+++ helper/utils.py
@@ -0,0 +1,34 @@
+
+from pathlib import Path
+import os
+import csv
+
+_DATA_FOLDER = "data"
+
+_DATA_CONVERSION_MAP = {
+    "str": str,
+    "int": int,
+    "float": float,
+}
+
+
+def get_project_root():
+    return str(Path().absolute().parent)
+
+
+def read_csv(file_name: str, field_convert_map: dict = {},):
+    file_path = os.path.join(get_project_root(), _DATA_FOLDER, file_name)
+    with open(file_path, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        field_names = csv_reader.fieldnames
+        csv_dict = {}
+        for row in csv_reader:
+            for field in field_names:
+                val = row.get(field, "")
+
+                if field in field_convert_map.keys():
+                    val = _DATA_CONVERSION_MAP[field_convert_map[field]](val)
+
+                csv_dict[field] = csv_dict.get(field, []) + [val]
+
+    return csv_dict
--- knn/knn.py
+++ knn/knn.py
@@ -1,7 +1,31 @@
 
+from helper import utils
+
+_DEFAULT_DATA = "iris.csv"
+
+_DEFAULT_DATA_SCHEMA = {
+    "Sepal.Length": "float",
+    "Sepal.Width": "float",
+    "Petal.Length": "float",
+    "Petal.Width": "float",
+    "Species": "str",
+}
+
+
+def load_df(file_name: str, schema: dict = {},):
+    df_dict = utils.read_csv(
+        file_name=file_name,
+        field_convert_map=schema,
+    )
+    return df_dict
+
 
 def knn():
-    return
+    df_dict = load_df(
+        file_name=_DEFAULT_DATA,
+        schema=_DEFAULT_DATA_SCHEMA,
+    )
+    return df_dict
 
 
 if __name__ == "__main__":
